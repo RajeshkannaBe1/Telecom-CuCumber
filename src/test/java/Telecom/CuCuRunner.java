937b989572a5d441c4669ff3e200574a8c2ecd6b
RajeshkannaBe1 <rajeshkannabe1@outlook.com>
2019-03-20 09:05:38 +0530

    TeleCom Process Finally Completed

--- src/test/java/Telecom/CuCuRunner.java
+++ src/test/java/Telecom/CuCuRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features",tags= {"@Blank"},plugin = {"html:target"},monochrome=(true),dryRun=(false),glue="Telecom")
+@CucumberOptions(features="src/test/resources/Features",plugin = {"html:target"},monochrome=(true),dryRun=(false),glue="Telecom")
 public class CuCuRunner {
 	
 
